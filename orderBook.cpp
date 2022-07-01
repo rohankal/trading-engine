0207d5ff19adfd67d676d46f1d621b16459f33b3
Rohan Kalra <rohankal@usc.edu>
2022-06-30 23:53:23 -0400

    set up classes

--- orderBook.cpp
+++ orderBook.cpp
@@ -1,9 +1,17 @@
 #include <imports.h>
+#include <order.cpp>
 
 class orderBook
 {
 private:
+    order buyOrders[];
+    order sellOrders[];
+
 public:
     orderBook();
     ~orderBook();
+    void addBuyOrder(order o){};
+    void addSellOrder(order o){};
+    void cancelBuyOrder(int id){};
+    void cancelSellOrder(int id){};
 };
